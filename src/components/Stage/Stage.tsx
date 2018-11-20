672681bf5b2fb87f89a833c8283322dfade5ab9a
Aivirth <aivirth.work@gmail.com>
2018-11-20 11:58:14 +0100

    Added first styles to Stage component

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -4,46 +4,48 @@ class Stage extends Component {
   render() {
     return (
       <div className="Stage">
-        <div className="Stage__actor">Actor</div>
+        <div className="Stage__wrapper">
+          <div className="Stage__actor">Actor</div>
 
-        <div className="Stage__options">
-          <ul className="Stage__tabs">
-            <li className="Stage__tab">
-              <a href="#test4" className="active">
-                Test 1
-              </a>
-            </li>
-            <li className="Stage__tab">
-              <a className="" href="#test5">
-                Test 2
-              </a>
-            </li>
-            <li className="Stage__tab">
-              <a href="#test6" className="">
-                Test 3
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="Stage__content">
-          <div id="test4" style={{ display: "block" }} className="active">
-            Test 1
+          <div className="Stage__options">
+            <ul className="Stage__tabs">
+              <li className="Stage__tab">
+                <a href="#test4" className="Stage__tab__link active">
+                  Test 1
+                </a>
+              </li>
+              <li className="Stage__tab">
+                <a className="Stage__tab__link" href="#test5">
+                  Test 2
+                </a>
+              </li>
+              <li className="Stage__tab">
+                <a href="#test6" className="Stage__tab__link">
+                  Test 3
+                </a>
+              </li>
+            </ul>
           </div>
-          <div id="test5" className="" style={{ display: "none" }}>
-            Test 2
-          </div>
-          <div id="test6" style={{ display: "none" }} className="">
-            Test 3
-          </div>
-        </div>
 
-        <div className="Stage__reveal">
-          <span className="Stage__title">
-            Card Title<i className="">close</i>
-          </span>
+          <div className="Stage__content">
+            <div id="test4" style={{ display: "block" }} className="active">
+              Test 1
+            </div>
+            <div id="test5" className="" style={{ display: "none" }}>
+              Test 2
+            </div>
+            <div id="test6" style={{ display: "none" }} className="">
+              Test 3
+            </div>
+          </div>
 
-          <button>Get Styles</button>
+          <div className="Stage__reveal">
+            <span className="Stage__title">
+              Card Title<i className="">close</i>
+            </span>
+
+            <button>Get Styles</button>
+          </div>
         </div>
       </div>
     );
--- src/containers/Layout/Main/Main.tsx
+++ src/containers/Layout/Main/Main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Stage from "../../../components/Stage/Stage";
 
 export default function Main(props: any) {
   return (
     <div className="Main d-flex container">
       <div className="Main__column">Current Options</div>
-      <div className="Main__column">Stage (Actor + Spectator)</div>
+      <div className="Main__column">
+        <Stage />
+      </div>
     </div>
   );
 }
