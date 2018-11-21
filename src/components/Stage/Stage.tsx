2594e03b29f3efeca735e7567f1e726e3e12c675
Aivirth <aivirth.work@gmail.com>
2018-11-21 11:46:41 +0100

    Finished Stage working prototype

--- src/components/Stage/Stage.tsx
+++ src/components/Stage/Stage.tsx
@@ -1,50 +1,57 @@
 import React, { Component } from "react";
 
 class Stage extends Component {
+  state = {
+    isRevealActive: false
+  };
+
+  onTabOptionClickHandler = () => {
+    this.setState({ isRevealActive: true });
+  };
+
+  onRevealCloseButtonClickHandler = () => {
+    this.setState({ isRevealActive: false });
+  };
+
   render() {
+    const { isRevealActive } = this.state;
+
     return (
       <div className="Stage">
         <div className="Stage__wrapper">
           <div className="Stage__actor">Actor</div>
 
+          <div
+            className={`Stage__reveal ${
+              isRevealActive ? "Stage__reveal--active" : ""
+            }`}
+          >
+            <div className="Stage__reveal__content">
+              <textarea name="" id="" />
+              <button>Get Styles</button>
+            </div>
+
+            <div className="Stage__reveal__footer">
+              <button
+                className="Stage__closeBtn"
+                onClick={this.onRevealCloseButtonClickHandler}
+              >
+                CLOSE
+              </button>
+            </div>
+          </div>
+
           <div className="Stage__options">
             <ul className="Stage__tabs">
               <li className="Stage__tab">
-                <a href="#test4" className="Stage__tab__link active">
-                  Test 1
-                </a>
-              </li>
-              <li className="Stage__tab">
-                <a className="Stage__tab__link" href="#test5">
-                  Test 2
-                </a>
-              </li>
-              <li className="Stage__tab">
-                <a href="#test6" className="Stage__tab__link">
-                  Test 3
-                </a>
+                <button
+                  className="Stage__tab__link active"
+                  onClick={this.onTabOptionClickHandler}
+                >
+                  GET STYLES
+                </button>
               </li>
             </ul>
-          </div>
-
-          <div className="Stage__content">
-            <div id="test4" style={{ display: "block" }} className="active">
-              Test 1
-            </div>
-            <div id="test5" className="" style={{ display: "none" }}>
-              Test 2
-            </div>
-            <div id="test6" style={{ display: "none" }} className="">
-              Test 3
-            </div>
-          </div>
-
-          <div className="Stage__reveal">
-            <span className="Stage__title">
-              Card Title<i className="">close</i>
-            </span>
-
-            <button>Get Styles</button>
           </div>
         </div>
       </div>
