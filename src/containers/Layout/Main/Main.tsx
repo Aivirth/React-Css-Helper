398491f1cc240e42d76c26d0322dee1f88aa3907
Aivirth <aivirth.work@gmail.com>
2018-11-21 16:19:45 +0100

    Added prototype checkbox

--- src/containers/Layout/Main/Main.tsx
+++ src/containers/Layout/Main/Main.tsx
@@ -4,7 +4,27 @@ import Stage from "../../../components/Stage/Stage";
 export default function Main(props: any) {
   return (
     <div className="Main d-flex container">
-      <div className="Main__column">Current Options</div>
+      <div className="Main__column">
+        {/* Temp Inputs */}
+        <div style={{ marginBottom: "30px" }} />
+
+        <input
+          className="inp-cbx"
+          id="cbx"
+          type="checkbox"
+          style={{ display: "none" }}
+        />
+        <label className="cbx" htmlFor="cbx">
+          <span>
+            <svg width="12px" height="10px" viewBox="0 0 12 10">
+              <polyline points="1.5 6 4.5 9 10.5 1" />
+            </svg>
+          </span>
+          <span>CodePenChallenge</span>
+        </label>
+
+        <hr />
+      </div>
       <div className="Main__column">
         <Stage />
       </div>
