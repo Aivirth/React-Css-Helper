f5817f4833ac6336289d0346db0b647814484a21
Aivirth <aivirth.work@gmail.com>
2018-12-10 17:52:18 +0100

    converted Link into NavLink in Sidenav.js

--- src/containers/Sidenav/Sidenav.js
+++ src/containers/Sidenav/Sidenav.js
@@ -1,29 +1,45 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NavItem from "../../components/Navbar/NavItem/NavItem";
-import { Link } from "react-router-dom";
+import { NavLink as Link } from "react-router-dom";
 
 export default function Sidenav() {
   return (
     <div className="Sidenav">
       <ul className="Sidenav__nav">
         <li className="Sidenav__item">
-          <Link to="/base" className="Sidenav__link">
+          <Link
+            to="/base"
+            className="Sidenav__link"
+            activeClassName="Sidenav__link--active"
+          >
             Base
           </Link>
         </li>
         <li className="Sidenav__item">
-          <Link to="/borders" className="Sidenav__link Sidenav__link--active">
+          <Link
+            to="/borders"
+            className="Sidenav__link"
+            activeClassName="Sidenav__link--active"
+          >
             Borders
           </Link>
         </li>
         <li className="Sidenav__item">
-          <Link to="/border-radius" className="Sidenav__link">
+          <Link
+            to="/border-radius"
+            className="Sidenav__link"
+            activeClassName="Sidenav__link--active"
+          >
             Border radius
           </Link>
         </li>
         <li className="Sidenav__item">
-          <Link to="/box-shadow" className="Sidenav__link">
+          <Link
+            to="/box-shadow"
+            className="Sidenav__link"
+            activeClassName="Sidenav__link--active"
+          >
             Box Shadow
           </Link>
         </li>
