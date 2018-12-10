2b6d57e48122880f4843e872920fb4990795e298
Aivirth <aivirth.work@gmail.com>
2018-12-10 17:33:23 +0100

    Added placeholder Options Components

---
+++ src/components/Options/BorderRadius/BorderRadius.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class BorderRadius extends Component {
+  render() {
+    return <div>Border Radius</div>;
+  }
+}
---
+++ src/components/Options/BoxShadow/BoxShadow.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class BoxShadow extends Component {
+  render() {
+    return <div>Box Shadow</div>;
+  }
+}
