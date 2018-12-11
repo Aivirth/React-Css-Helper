d329b6d3c8c0fe6cd10e38b55a5925f6276dcd3f
Aivirth <aivirth.work@gmail.com>
2018-12-11 11:57:22 +0100

    Corrected typos in borderRadiusReducer

--- src/store/reducers/borderRadiusReducer.js
+++ src/store/reducers/borderRadiusReducer.js
@@ -17,42 +17,42 @@ const initialState = {
   borderTopRightRadius: {
     inputType: "range",
     elementConfig: {
-      label: "Border Top-Left Radius"
+      label: "Border Top-Right Radius"
     },
     htmlProperties: {
       type: "range",
       step: "1",
       min: 0,
       max: 300,
-      name: "borderTopLeftRadius"
+      name: "borderTopRightRadius"
     },
     radii: { radiusX: 50, radiusY: 20 }
   },
   borderBottomLeftRadius: {
     inputType: "range",
     elementConfig: {
-      label: "Border Top-Left Radius"
+      label: "Border Bottom-Left Radius"
     },
     htmlProperties: {
       type: "range",
       step: "1",
       min: 0,
       max: 300,
-      name: "borderTopLeftRadius"
+      name: "borderBottomLeftRadius"
     },
     radii: { radiusX: 50, radiusY: 20 }
   },
   borderBottomRightRadius: {
     inputType: "range",
     elementConfig: {
-      label: "Border Top-Left Radius"
+      label: "Border Bottom-Right Radius"
     },
     htmlProperties: {
       type: "range",
       step: "1",
       min: 0,
       max: 300,
-      name: "borderTopLeftRadius"
+      name: "borderBottomRightRadius"
     },
     radii: { radiusX: 50, radiusY: 20 }
   }
