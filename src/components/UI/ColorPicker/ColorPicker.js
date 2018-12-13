4e2f96a3d951584106ad0b3f5ca0a868ecbe68b3
Aivirth <aivirth.work@gmail.com>
2018-12-13 11:50:52 +0100

    Added addiotional styling and layout structure to inputs

--- src/components/UI/ColorPicker/ColorPicker.js
+++ src/components/UI/ColorPicker/ColorPicker.js
@@ -3,11 +3,20 @@ import React from "react";
 export default props => {
   const { changed, id, value } = props;
   return (
-    <div>
-      <label htmlFor={id}>
+    <div className="Inputs__wrapper">
+      <label htmlFor={id} className="Inputs__label">
         Color: <strong>{value}</strong>
       </label>
-      <input type="color" id={id} name={id} onChange={changed} value={value} />
+      <div className="Inputs__control">
+        <input
+          className="Inputs__color"
+          type="color"
+          id={id}
+          name={id}
+          onChange={changed}
+          value={value}
+        />
+      </div>
     </div>
   );
 };
