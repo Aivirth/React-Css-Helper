a1229f7d64e31ad22e210bf3204adeda08fa9327
Aivirth <aivirth.work@gmail.com>
2018-12-14 17:26:08 +0100

    hooked Borders component to state

--- src/components/Options/Borders/Borders.js
+++ src/components/Options/Borders/Borders.js
@@ -1,11 +1,93 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import Input from "../../UI/Input";
 
-export default class Borders extends Component {
+class Borders extends Component {
+  componentDidMount() {
+    console.log("Borders:", this.props);
+  }
+
+  inputChangeHandler = (e, identifier) => {
+    console.log(e, identifier);
+  };
+
   render() {
+    const { stylesFromState } = this.props;
+
+    // console.log("Borders", stylesFromState);
+
+    let propertiesArray = [];
+
+    for (let key in stylesFromState) {
+      propertiesArray.push({
+        id: key,
+        config: this.props.stylesFromState[key]
+      });
+    }
+
+    const formattedData = [];
+
+    propertiesArray.forEach(property => {
+      const newArrFormat = {
+        id: property.id,
+        properties: [
+          property.config.style,
+          property.config.color,
+          property.config.width
+        ]
+      };
+
+      formattedData.push(newArrFormat);
+    });
+
+    // console.log(propertiesArray);
+    // console.log(formattedData);
+
     return (
-      <div>
+      <React.Fragment>
         <h2 className="Inputs__title">Borders</h2>
-      </div>
+
+        {formattedData.map(border => {
+          return (
+            <React.Fragment key={border.id}>
+              <h3>{border.id}</h3>
+              {border.properties.map(property => {
+                const {
+                  htmlProperties,
+                  elementConfig,
+                  inputType,
+                  value
+                } = property;
+
+                return (
+                  <Input
+                    key={htmlProperties.name}
+                    inputType={inputType}
+                    htmlProperties={htmlProperties}
+                    changed={e => this.inputChangeHandler(e, border.id)}
+                    elementConfig={elementConfig}
+                    label={elementConfig.label}
+                    value={value}
+                  />
+                );
+              })}
+            </React.Fragment>
+          );
+        })}
+      </React.Fragment>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    stylesFromState: {
+      top: state.borders.top,
+      bottom: state.borders.bottom,
+      right: state.borders.right,
+      left: state.borders.left
+    }
+  };
+};
+
+export default connect(mapStateToProps)(Borders);
