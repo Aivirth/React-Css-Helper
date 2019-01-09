74a5d7214f5f967518a76d7b25cba50edc7363dd
Aivirth <aivirth.work@gmail.com>
2019-01-09 11:14:55 +0100

    Added responsiveness to Sidebar

--- src/containers/Sidenav/Sidenav.js
+++ src/containers/Sidenav/Sidenav.js
@@ -1,47 +1,56 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import { NavLink as Link } from "react-router-dom";
 
-export default function Sidenav() {
-  return (
-    <div className="Sidenav">
-      <ul className="Sidenav__nav">
-        <li className="Sidenav__item">
-          <Link
-            to="/base"
-            className="Sidenav__link"
-            activeClassName="Sidenav__link--active"
-          >
-            Base
-          </Link>
-        </li>
-        <li className="Sidenav__item">
-          <Link
-            to="/borders"
-            className="Sidenav__link"
-            activeClassName="Sidenav__link--active"
-          >
-            Borders
-          </Link>
-        </li>
-        <li className="Sidenav__item">
-          <Link
-            to="/border-radius"
-            className="Sidenav__link"
-            activeClassName="Sidenav__link--active"
-          >
-            Border radius
-          </Link>
-        </li>
-        <li className="Sidenav__item">
-          <Link
-            to="/box-shadow"
-            className="Sidenav__link"
-            activeClassName="Sidenav__link--active"
-          >
-            Box Shadow
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+class Sidenav extends Component {
+  state = {
+    navLinks: [
+      { url: "/base", anchor: "Base" },
+      { url: "/borders", anchor: "Borders" },
+      { url: "/border-radius", anchor: "Border Radius" },
+      { url: "/box-shadow", anchor: "Box Shadow" }
+    ],
+    isOpen: false
+  };
+
+  toggleSidenavStatus = e => {
+    e.preventDefault();
+
+    if (window.innerWidth <= 900) {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+  };
+
+  render() {
+    const { navLinks, isOpen } = this.state;
+    const togglerIcon = isOpen ? faTimes : faBars;
+    const sidenavStatus = isOpen ? " Sidenav--open" : "";
+
+    return (
+      <div className={`Sidenav ${sidenavStatus}`}>
+        <ul className="Sidenav__nav">
+          {navLinks.map(navLink => (
+            <li className="Sidenav__item" key={navLink.anchor}>
+              <Link
+                to={navLink.url}
+                className="Sidenav__link"
+                activeClassName="Sidenav__link--active"
+              >
+                {navLink.anchor}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="Sidenav__toggler" onClick={this.toggleSidenavStatus}>
+          <FontAwesomeIcon icon={togglerIcon} />
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Sidenav;
