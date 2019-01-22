dcc973caa7aa75c2eb0857cd8b73004f2f7df3e9
Aivirth <aivirth.work@gmail.com>
2019-01-22 11:51:18 +0100

    Added Footer infos

--- src/containers/Layout/Footer/Footer.js
+++ src/containers/Layout/Footer/Footer.js
@@ -6,16 +6,24 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
   return (
     <div className="Footer">
-      <div className="Footer__block">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab fuga quam
-        veniam perferendis tenetur optio aspernatur quibusdam totam inventore
-        suscipit magni vel,
+      <div className="Footer__block Footer__infos">
+        Author:{" "}
+        <a
+          className="Footer__link"
+          href="https://github.com/Aivirth"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aivirth
+        </a>{" "}
+        2019 - IMT License
       </div>
-      <div className="Footer__block">
+      <div className="Footer__block Footer__actions">
         <a
           className="Footer__btn "
           href="https://github.com/Aivirth/React-Css-Helper"
-          target="_black"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Fork me on Github <FontAwesomeIcon icon={faGithub} />
         </a>
